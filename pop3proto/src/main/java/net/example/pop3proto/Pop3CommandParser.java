c3aed4fc00bc854f3332f3538f55e519fda0a2cf
VolLol <nurs.ypurs@gmail.com>
2020-05-09 15:43:44 +0300

    Add Pattern for TOP command

--- pop3proto/src/main/java/net/example/pop3proto/Pop3CommandParser.java
+++ pop3proto/src/main/java/net/example/pop3proto/Pop3CommandParser.java
@@ -11,26 +11,20 @@ public class Pop3CommandParser {
 
     final private static Pattern noopCommandPattern = Pattern.compile("^(?i)(noop)(?-i)$");
     final private static Pattern retrCommandPattern = Pattern.compile("^(?i)Retr(?-i)\\s(\\d+)$");
+    final private static Pattern topCommandPattern = Pattern.compile("^(?i)Top(?-i)\\s(\\d+)\\s(\\d+)$");
 
     public static Pop3Command parse(String line) {
-        Matcher m = retrCommandPattern.matcher(line);
-        if (chekForNoopCommand(line)) {
+        Matcher m = noopCommandPattern.matcher(line);
+        if (m.matches()) {
             return new Pop3CommandNoop();
-        } else if (m.matches()) {
+        } else if (m.usePattern(retrCommandPattern).matches()) {
             int mailIndex = Integer.parseInt(m.group(1));
             return new Pop3CommandRetr(mailIndex);
+        } else if (m.usePattern(topCommandPattern).matches()) {
+            return new Pop3CommandTop(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)));
         }
 
         throw new Pop3UnknownCommand("Unknown Command");
     }
 
-    private static boolean chekForNoopCommand(String line) {
-        Matcher m = noopCommandPattern.matcher(line);
-        return m.matches();
-    }
-
-    private static boolean checkForRetrCommand(String line) {
-        Matcher m = retrCommandPattern.matcher(line);
-        return m.matches();
-    }
 }
